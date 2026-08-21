724f1db18922196ae2bca2142d54e376f16aa6a3
jagr2808 <jacob.grevstad@gmail.com>
2026-08-21 13:42:15 +0200

    updating database from /log-appointment/

---
+++ ClipSnip/Controllers/AppointmentLoggingController.cs
@@ -0,0 +1,39 @@
+﻿using ClipSnip.Data;
+using ClipSnip.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClipSnip.Controllers;
+
+[Route("log-appointment")]
+public class AppointmentLoggingController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("log")]
+    public async Task<IActionResult> LogAppointment([FromBody] AppointmentRequest appointment)
+    {
+        Console.WriteLine("Postrequist");
+        Console.WriteLine(appointment.date);
+        var user = await userManager.GetUserAsync(User);
+        var app = new Appointment();
+        app.UserId = user.Id;
+        Console.WriteLine($"duration = {appointment.duration}");
+        app.DurationInMinutes = appointment.duration;
+        app.TimeOfAppointment = appointment.date;
+        user.Appointments.Add(app);
+        await db.SaveChangesAsync();
+
+        return View();
+    }
+}
+
+public class AppointmentRequest
+{
+    public int duration { get; set; }
+    public DateTime date { get; set; }
+}
---
+++ ClipSnip/wwwroot/js/appointmentlogging.js
@@ -0,0 +1,24 @@
+﻿const submitButton = document.getElementById("submitButton");
+const mins = document.getElementById("durationInMinutes");
+const date = document.getElementById("dateOfAppointment");
+const hairstyle = document.getElementById("hairstyle");
+
+submitButton.addEventListener('click', async () => {
+    const data = {
+        duration: mins.value,
+        date: date.value
+    };
+
+    try {
+        const response = await fetch("/log-appointment/log", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+    }
+    catch (error) {
+        status.textContent = error.message;
+    }
+});
